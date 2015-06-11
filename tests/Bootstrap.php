db2b55548c91a3050aaec899e975589634fad95f
Jaspaul Bola <jaspaul.b@gmail.com>
2015-06-11 08:58:42 -0400

    Initial Commit

---
+++ tests/Bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+if (!@include_once __DIR__ . '/../vendor/autoload.php') {
+	exit("Missing project dependencies.");
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php namespace Katas\RomanNumerals\Test;
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * Runs prior to every test in the suite.
+	 */
+	public function setUp()
+	{
+	}
+
+	/**
+	 * Runs after every test in the suite.
+	 */
+	protected function tearDown()
+	{
+	}
+
+}
