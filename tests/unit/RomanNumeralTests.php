c091e5446cc27a7aea9e6056d9135014ce0115d3
Jaspaul Bola <jaspaul.b@gmail.com>
2015-06-15 19:08:30 -0400

    Refactor tests, add test for III

--- tests/unit/RomanNumeralTests.php
+++ tests/unit/RomanNumeralTests.php
@@ -4,17 +4,26 @@ use Katas\RomanNumerals\Tests\TestCase;
 use Katas\RomanNumerals\NumberConverter;
 
 class RomanNumeralTests extends TestCase {
+	protected $converter;
+
+	public function setUp() {
+		parent::setUp();
+		$this->converter = new NumberConverter();
+	}
 
 	public function test_1_converts_to_I() {
-		$converter = new NumberConverter();
-		$romanNumeral = $converter->arabicToRoman(1);
+		$romanNumeral = $this->converter->arabicToRoman(1);
 		$this->assertEquals('I', $romanNumeral);
 	}
 
 	public function test_2_converts_to_II() {
-		$converter = new NumberConverter();
-		$romanNumeral = $converter->arabicToRoman(2);
+		$romanNumeral = $this->converter->arabicToRoman(2);
 		$this->assertEquals('II', $romanNumeral);
+	}
+
+	public function test_3_converts_to_III() {
+		$romanNumeral = $this->converter->arabicToRoman(3);
+		$this->assertEquals('III', $romanNumeral);
 	}
 
 }
