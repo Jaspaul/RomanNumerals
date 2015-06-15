e6a63c374b8cf4c9efd7cd23f9186478486998f9
Jaspaul Bola <jaspaul.b@gmail.com>
2015-06-15 19:06:03 -0400

    Add test for 2 conversion to roman numeral

--- tests/unit/RomanNumeralTests.php
+++ tests/unit/RomanNumeralTests.php
@@ -5,10 +5,16 @@ use Katas\RomanNumerals\NumberConverter;
 
 class RomanNumeralTests extends TestCase {
 
-	public function test_one_converts_to_I() {
+	public function test_1_converts_to_I() {
 		$converter = new NumberConverter();
 		$romanNumeral = $converter->arabicToRoman(1);
 		$this->assertEquals('I', $romanNumeral);
+	}
+
+	public function test_2_converts_to_II() {
+		$converter = new NumberConverter();
+		$romanNumeral = $converter->arabicToRoman(2);
+		$this->assertEquals('II', $romanNumeral);
 	}
 
 }
