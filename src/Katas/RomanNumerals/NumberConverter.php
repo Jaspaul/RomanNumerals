3e0f8cd0663b71a32c8ded067901e71db6495e1c
Jaspaul Bola <jaspaul.b@gmail.com>
2015-06-16 18:59:26 -0400

    README support for larger numbers

--- src/Katas/RomanNumerals/NumberConverter.php
+++ src/Katas/RomanNumerals/NumberConverter.php
@@ -3,12 +3,15 @@
 class NumberConverter {
 
 	private $map = [
-		100 => 'C',
-		10  => 'X',
-		9   => 'IX',
-		5   => 'V',
-		4   => 'IV',
-		1   => 'I'
+		1000 => 'M',
+		500  => 'D',
+		100  => 'C',
+		50   => 'L',
+		10   => 'X',
+		9    => 'IX',
+		5    => 'V',
+		4    => 'IV',
+		1    => 'I'
 	];
 
 	public function arabicToRoman($number) {
